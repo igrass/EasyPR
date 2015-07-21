32fa5bd143c7a6354b9048a96801c16754482ed0
zac <494511292@qq.com>
2015-07-21 17:25:55 +0800

    remove plateRecognize's  index param

--- test/accuracy.hpp
+++ test/accuracy.hpp
@@ -72,7 +72,7 @@ int accuracyTest(const char* test_path) {
     cout << "原牌:" << plateLicense << endl;
 
     vector<string> plateVec;
-    int result = pr.plateRecognize(src, plateVec, i);
+    int result = pr.plateRecognize(src, plateVec);
     if (result == 0) {
       int num = plateVec.size();
 
